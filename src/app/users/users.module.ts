8c44160f33652a853657236db2b94af37fafdc2c
Kiran Gudla <Kiran_Gudla@epam.com>
2019-12-14 06:48:09 +0530

    clean up

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -2,10 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { Routes, RouterModule } from '@angular/router';
-import { UserComponent } from './user/user.component';
 import { HeaderComponent } from './header/header.component';
 import { DetailsCardComponent } from './details-card/details-card.component';
-import { DetailsTableComponent } from './details-table/details-table.component';
 import { GitthubApiService } from './gitthub-api.service';
 import { ReactiveFormsModule } from '@angular/forms';
 import { StoreModule } from '@ngrx/store';
@@ -18,12 +16,11 @@ export const userRoutes: Routes = [
   {
     path: '',
     component: HomeComponent
-
   }
 ];
 
 @NgModule({
-  declarations: [HomeComponent, UserComponent, HeaderComponent, DetailsCardComponent, DetailsTableComponent ],
+  declarations: [HomeComponent, HeaderComponent, DetailsCardComponent ],
   imports: [
     CommonModule,
     HttpClientModule,
@@ -33,8 +30,7 @@ export const userRoutes: Routes = [
     EffectsModule.forFeature([UsersEffects])
   ],
   exports: [
-    HomeComponent,
-    UserComponent
+    HomeComponent
   ],
   providers: [GitthubApiService]
 })
